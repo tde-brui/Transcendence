2effa30d00b6e23f6f6bd1596ee5d2436b48839f
dvan-kle <daniel@vkleef.nl>
2024-10-09 18:37:31 +0200

    Api test

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+console.log('Client ID:', clientId);
+console.log('Client Secret:', clientSecret);
+
+async function getAccessToken() {
+    try {
+        const tokenResponse = await axios.post('https://api.intra.42.fr/oauth/token', null, {
+            params: {
+                grant_type: 'client_credentials',
+                client_id: clientId,
+                client_secret: clientSecret,
+            },
+        });
+        const accessToken = tokenResponse.data.access_token;
+        return accessToken;
+    } catch (error) {
+        console.error('Error fetching access token:', error);
+    }
+}
+
+async function getUsersInCursus() {
+    const token = await getAccessToken();
+    const response = await axios.get('https://api.intra.42.fr/v2/users/dvan-kle', {
+        headers: { Authorization: `Bearer ${token}` },
+    });
+    const usersInCursus = response.data;
+	// const userID = usersInCursus.id;
+	// const login = usersInCursus.login;
+	// console.log('User ID:', userID);
+	// console.log('Login:', login);
+    console.log(usersInCursus);
+}
+
+getUsersInCursus();
+
