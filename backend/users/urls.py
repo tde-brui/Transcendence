47442a82e67f622f9750d998e0cd81969a58fa75
Tijmen De Bruine <tijmendebruinebusiness@gmail.com>
2024-11-26 19:18:10 +0100

    testing cookies

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 urlpatterns = [
 	#path('', views.user_list),
-	path("login/", views.user_login.as_view()),
+	path('login/', views.user_login.as_view()),
 	path('register/', views.user_register.as_view()),
-	path("verify_otp/", views.verify_otp.as_view()),
+	path('verify_otp/', views.verify_otp.as_view()),
 	path('auth/verify/', views.check_token.as_view()),
 	path('<int:pk>/', views.user_detail),
 ]
