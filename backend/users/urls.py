f2f7eb6e398374f77503fdfe275f6c203032c0f8
StijnScheltinga <stijnskoop@gmail.com>
2024-12-04 14:16:25 +0100

    added get /users/ url again

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,13 +2,13 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
+	path('', views.get_users.as_view()),
 	path('login/', views.user_login.as_view()),
 	path('register/', views.user_register.as_view()),
 	path('<int:pk>/', views.user_detail.as_view()),
 	path('verify_otp/', views.verify_otp.as_view()),
 	path('auth/verify/', views.check_token.as_view()),
 	path('me/', views.get_logged_in_user.as_view()),
-	path('<int:pk>/', views.user_detail.as_view()),
 	path('42_login/', views.user_42_login),
 	path('42_callback/', views.user_42_callback),
 ]
